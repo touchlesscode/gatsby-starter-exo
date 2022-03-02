8f55caf3403fd5e1a07e35126716077ba8d15e6a
Touchless Inc <info@touchlessinc.com>
2022-03-02 12:04:40 -0500

    icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
   }, "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/icons/icon.png"
     }
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
