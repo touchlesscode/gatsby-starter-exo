483518a6e2dd623f75d09ba832c528af2b2eec07
Touchless Inc <info@touchlessinc.com>
2022-03-02 11:13:52 -0500

    initializing

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     resolve: 'gatsby-source-sanity',
     options: {
       "projectId": "ju3h49o8",
-      "dataset": ""
+      "dataset": "production"
     }
   }, "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
     resolve: 'gatsby-plugin-manifest',
@@ -20,5 +20,25 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
+  },
+  {
+    resolve: `gatsby-theme-exo`,
+  },
+  'gatsby-plugin-theme-ui',
+  {
+    resolve: "gatsby-omni-font-loader",
+    options: {
+      mode: "async",
+      enableListener: true,
+      interval: 400,
+      preconnect: ["https://fonts.gstatic.com"],
+      web: [{
+        /* Exact name of the font as defied in @font-face CSS rule */
+        name: "Work Sans",
+        /* URL to the font CSS file with @font-face definition */
+        file: "https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600&display=swap",
+      },
+      ],
+    }
   }]
 };
